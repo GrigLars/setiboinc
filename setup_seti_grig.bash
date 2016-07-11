2f7545140c3f33b657649a5867f4eac1adfed5ba
Grig Larson <GrigLars@users.noreply.github.com>
2016-07-11 17:08:13 -0400

    Create setup_seti_grig.bash

---
+++ setup_seti_grig.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BOINC_ROOT="/opt"
+BOINC_PATH="/opt/BOINC"
+BOINC_VERSION="7.2.42"
+
+# Must run as root
+
+# Check machine version
+MACHINE=$(uname -m)
+
+# Libraries for Debian 8
+# apt-get install libx11-6
+# apt-get install libxss1
+
+
+
+# Download right client
+
+# Set up service
+# systemctl enable boinc
+
+# Set up client
