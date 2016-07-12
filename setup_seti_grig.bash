029876941b679dfb1398e8dc03872cde5c19b553
Grig Larson <GrigLars@users.noreply.github.com>
2016-07-12 13:38:11 -0400

    Placemarkers for changing user eventually

--- setup_seti_grig.bash
+++ setup_seti_grig.bash
@@ -4,7 +4,14 @@ BOINC_ROOT="/opt"
 BOINC_PATH="${BOINC_ROOT}/BOINC"
 BOINC_VERSION="7.2.42"
 
-# Must run as root
+# Must be run as root
+# This is kind of unsafe: we need to run these as a boinc user
+if [ "$(whoami)" != 'root' ]; then
+        echo -e "\e[31;1m$0: ERROR: You have no permission to run $0 as non-root user.\e[0m"
+        exit 1;
+fi
+
+# useradd -r -m /opt/BOINC -s /bin/false boinc
 
 # Check machine version
 MACHINE=$(uname -m)
@@ -12,6 +19,7 @@ MACHINE=$(uname -m)
 # Libraries for Debian 8
 # apt-get install libx11-6 libxss1
 
+# Libraries for CentOS
 
 # Download the init script
 wget https://raw.githubusercontent.com/GrigLars/setiboinc/master/boinc_init -O /etc/init.d/boinc
@@ -27,10 +35,14 @@ else
 fi
 
 sh boinc_*
+# chown -R boinc:boinc ${BOINC_PATH}
 cd ${BOINC_PATH}
+
 
 # Set up service
 
 # systemctl enable boinc
 
 # Set up client
+/etc/init.d/boinc attach
+tail -f /var/log/boinc.log
