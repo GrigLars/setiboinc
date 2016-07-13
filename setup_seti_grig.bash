61f04ea3e8b14e5f8dad644b6a5e7a05a7617af1
Grig Larson <GrigLars@users.noreply.github.com>
2016-07-13 10:10:52 -0400

    More or less made it Debian dependent

--- setup_seti_grig.bash
+++ setup_seti_grig.bash
@@ -11,18 +11,23 @@ if [ "$(whoami)" != 'root' ]; then
         exit 1;
 fi
 
-# useradd -r -m /opt/BOINC -s /bin/false boinc
-
 # Check machine version
 MACHINE=$(uname -m)
 
 # Libraries for Debian 8
-# apt-get install libx11-6 libxss1
+apt-get -y install libx11-6 libxss1 multitail
 
 # Libraries for CentOS
+# - Currently, due to shitty programming on static libssl stuff, I haven't gotten this to
+#   work on current CentOS
+
+if [ -f "/etc/debian_version"]; then 
+        echo -e "\e[31;1m$0: ERROR: This can only run on Debian.  Bummer.\e[0m"
+        exit 1;
+fi
 
 # Download the init script
-wget https://raw.githubusercontent.com/GrigLars/setiboinc/master/boinc_init -O /etc/init.d/boinc
+wget -nv https://raw.githubusercontent.com/GrigLars/setiboinc/master/boinc_init -O /etc/init.d/boinc
 
 chmod +x /etc/init.d/boinc
 mkdir -p ${BOINC_ROOT}
@@ -30,15 +35,15 @@ cd ${BOINC_ROOT}
 
 # Download right client
 if [ ${MACHINE} eq "i686" ]; then
-  wget http://boinc.berkeley.edu/dl/boinc_${BOINC_VERSION}_i686-pc-linux-gnu.sh
+  wget -nv http://boinc.berkeley.edu/dl/boinc_${BOINC_VERSION}_i686-pc-linux-gnu.sh
 else
-  wget http://boinc.berkeley.edu/dl/boinc_${BOINC_VERSION}_x86_64-pc-linux-gnu.sh
+  wget -nv http://boinc.berkeley.edu/dl/boinc_${BOINC_VERSION}_x86_64-pc-linux-gnu.sh
 fi
 
 sh boinc_*
+# useradd -r -m /opt/BOINC -s /bin/false boinc
 # chown -R boinc:boinc ${BOINC_PATH}
 cd ${BOINC_PATH}
-
 
 # Set up service
 
